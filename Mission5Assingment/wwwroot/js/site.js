8feade3409b0f6799742ab8a93fea31bbcc3302a
Andrew Malone <mandrew434@gmail.com>
2025-02-05 10:12:37 -0700

    Add project files.

---
+++ Mission5Assingment/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("#btn-calculate").click(function () {
+    $('#totalCharge').val(''); // Clear the value of totalCharge
+
+    let myRate = 1499.99;
+    let numHours = parseFloat($('#hours').val());
+
+    // Validation for number and positive number
+    if (!isNaN(numHours) && numHours > 0) {
+        let charge = (myRate * numHours).toFixed(2);
+        $('#totalCharge').val(charge);
+    }
+    else {
+        alert('Please enter a valid number of hours');
+    }
+})
